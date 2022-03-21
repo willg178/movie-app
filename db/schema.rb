d428f6ea9befef9ce03617b533a4ccad25e657bf
Will Grund <willgrund178@gmail.com>
2022-03-21 11:51:25 -0400

    adds migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_15_162701) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_21_153517) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_15_162701) do
     t.integer "year"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "director"
+    t.boolean "english"
   end
 
 end
